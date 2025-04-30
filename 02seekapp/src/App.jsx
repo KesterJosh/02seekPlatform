1dece4ec801a86eee01959c60f5af846cf0d8cb2
KesterJosh <adeyelukester2@gmail.com>
2025-04-30 09:38:23 +0100

    app development

---
+++ 02seekapp/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import growthLogo from './assets/growth.gif'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        
+        
+          <img src={growthLogo} className="logo react" alt="React logo" />
+      </div>
+      <h1>02 Seek Development Ongoing</h1>
+      
+    </>
+  )
+}
+
+export default App
